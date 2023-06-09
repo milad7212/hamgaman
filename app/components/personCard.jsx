d48d46d9dff2992a4c8cc5c5b6c2f8af624cc6d4
miillaadddddd <milad7212@gmail.com>
2023-06-09 18:31:34 +0330

    set condition for cards

--- app/components/personCard.jsx
+++ app/components/personCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import { BsFillBalloonHeartFill } from "react-icons/bs";
-import {GoComment} from "react-icons/go"
-
+import { GoComment } from "react-icons/go";
+import C from "classnames";
 function PersonCard({ data }) {
+  let classCard = C({
+    "ring-gray-900  ring-4": data.favorite,
+    
+  });
+  let classIcon = C({
+    "text-red-600": data.favorite,
+  });
   return (
-    <div className="bg-white  ring-4 ring-gray-900  rounded shadow-lg overflow-hidden">
+    <div
+      className={`bg-white     rounded shadow-lg overflow-hidden ${classCard}`}
+    >
       <div className="h-40 p-1 bg-gray-900 rounded-b-sm flex overflow-hidden relative">
         <div className="h-full aspect-square rounded-sm shadow-sm relative">
           <Image className="" src="/images/milad.jpeg" alt="My Image" fill />
@@ -16,8 +25,8 @@ function PersonCard({ data }) {
         <span className="text-gray-700 my-2">{data.skills.join(" |  ")}</span>
         <div className="flex items-center mt-4 justify-between">
           <div className="flex">
-            <BsFillBalloonHeartFill size={25} className='text-red-600' />
-            <GoComment size={25} className='text-gray-600 mr-4' />
+            <BsFillBalloonHeartFill size={25} className={ classIcon } />
+            <GoComment size={25} className="text-gray-600 mr-4" />
           </div>
           <a className="text-left my-2 text-gray-800" href="tel:09139939426">
             09139939426
