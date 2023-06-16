365217347749e0c272161541be2f09579ccf94ce
miillaadddddd <milad7212@gmail.com>
2023-06-17 00:08:23 +0330

    improve search page

--- app/components/personCard.jsx
+++ app/components/personCard.jsx
@@ -1,32 +1,56 @@
 import React from "react";
 import Image from "next/image";
-import { BsFillBalloonHeartFill } from "react-icons/bs";
-import { GoComment } from "react-icons/go";
+import { BsFillHeartFill } from "react-icons/bs";
+import { GoComment, GoNote } from "react-icons/go";
 import C from "classnames";
-function PersonCard({ data }) {
-  let classCard = C({
-    "ring-gray-900  ring-4": data.favorite,
-    
+
+function PersonCard({ data, clickOnHeart }) {
+  const classCard = C({
+    "ring-gray-900 ring-4": data.favorite,
   });
-  let classIcon = C({
+  const classIcon = C({
     "text-red-600": data.favorite,
   });
+
   return (
-    <div
-      className={`bg-white     rounded shadow-lg overflow-hidden ${classCard}`}
-    >
+    <div className={`bg-white rounded shadow-lg overflow-hidden ${classCard}`}>
       <div className="h-40 p-1 bg-gray-900 rounded-b-sm flex overflow-hidden relative">
         <div className="h-full aspect-square rounded-sm shadow-sm relative">
-          <Image className="" src="/images/milad.jpeg" alt="My Image" fill />
+          <Image
+            className="rounded-md border-l-gray-300 border-l-2"
+            src="/images/milad.jpeg"
+            alt="My Image"
+            fill
+          />
+        </div>
+        <div className="flex justify-around flex-col items-center w-[70%]">
+          <div className="flex items-center">
+            <BsFillHeartFill
+              size={25}
+              className="text-gray-200 cursor-pointer"
+            />
+            <span className="text-gray-200 mr-2">{data.hearts ?? 0}</span>
+          </div>
+          <div className="flex items-center">
+            <GoComment size={25} className="text-gray-200  cursor-pointer" />
+            <span className="text-gray-200 mr-2">{data.comments ?? 0}</span>
+          </div>
+          <div className="flex items-center">
+            <GoNote size={25} className="text-gray-200 cursor-pointer" />
+            <span className="text-gray-200 mr-2">{data.notes ?? 0}</span>
+          </div>
         </div>
       </div>
       <div className="p-3">
         <h2 className="text-lg font-bold">{data.name}</h2>
-        <span className="text-gray-700 my-2">{data.skills.join(" |  ")}</span>
+        <span className="text-gray-700 my-2">{data.skills.join(" | ")}</span>
         <div className="flex items-center mt-4 justify-between">
           <div className="flex">
-            <BsFillBalloonHeartFill size={25} className={ classIcon } />
-            <GoComment size={25} className="text-gray-600 mr-4" />
+            <BsFillHeartFill
+              onClick={() => clickOnHeart(data)}
+              size={25}
+              className={`${classIcon} cursor-pointer`}
+            />
           </div>
           <a className="text-left my-2 text-gray-800" href="tel:09139939426">
             09139939426
@@ -37,4 +61,4 @@ function PersonCard({ data }) {
   );
 }
 
-export default PersonCard;
+export default React.memo(PersonCard);
--- app/one/page.jsx
+++ app/one/page.jsx
@@ -1,9 +1,11 @@
+"use client"
+import { useCallback, useState } from "react";
 import Header from "../components/header";
 import PersonCard from "../components/personCard";
-
+import { RiFilter2Fill, RiArrowUpDownFill } from "react-icons/ri";
 
 function App() {
-  const people = [
+  const [dataPeople, setDataPeople] = useState([
     {
       id: 1,
       name: "میلاد حسنی",
@@ -12,12 +14,50 @@ function App() {
     },
     { id: 2,favorite:false, name: "سجاد حسنی", skills: ["داربست ", "قالب بندی"] },
     { id: 3,favorite:false,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
-    { id: 3,favorite:true,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
-    { id: 3,favorite:false,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
-    { id: 3,favorite:true,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
-    { id: 3,favorite:false,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
+    { id: 4,favorite:true,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
+    { id: 5,favorite:false,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
+    { id: 6,favorite:true,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
+    { id: 7,favorite:false,  name: "عباد حسنی", skills: ["طراحی نما", "پیمانکار"] },
     // Add more people data
-  ];
+  ])
+  const [filterFavorites, setFilterFavorites] = useState(false);
+  const [sortBy, setSortBy] = useState("");
+  const [isFilterPageOpen, setIsFilterPageOpen] = useState(false);
+
+  const clickOnHeart=useCallback(
+    selectedCard => {
+      const index=dataPeople.findIndex((person)=>person.id ===selectedCard.id);
+      if(index !==-1){
+        const updatedPeople =[...dataPeople];
+        updatedPeople[index].favorite=!updatedPeople[index].favorite;
+        setDataPeople(updatedPeople)
+      }
+    },
+    [dataPeople],
+  )
+  const handleFilterFavorites = () => {
+    setFilterFavorites((prevState) => !prevState);
+  };
+
+  const handleSortBy = () => {
+    if (sortBy === "asc") {
+      setSortBy("desc");
+    } else {
+      setSortBy("asc");
+    }
+  };
+
+  const filteredAndSortedPeople = dataPeople
+    .filter((person) => !filterFavorites || person.favorite)
+    .sort((a, b) => {
+      if (sortBy === "asc") {
+        return a.name.localeCompare(b.name);
+      } else if (sortBy === "desc") {
+        return b.name.localeCompare(a.name);
+      }
+      return 0;
+    });
+
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -28,16 +68,55 @@ function App() {
       {/* Main content */}
       <main className="flex-grow">
         <div className="container mx-auto py-4 px-6">
+        <div className="flex justify-between mb-4">
+            <button
+              onClick={handleFilterFavorites}
+              className="flex items-center space-x-1 px-3 py-1 bg-gray-900 text-white rounded-lg focus:outline-none"
+            >
+              <RiFilter2Fill size={18} />
+              <span>{filterFavorites ? "All" : "Favorites"}</span>
+            </button>
+            <button
+              onClick={handleSortBy}
+              className="flex items-center space-x-1 px-3 py-1 bg-gray-900 text-white rounded-lg focus:outline-none"
+            >
+              <RiArrowUpDownFill size={18} />
+              <span>{sortBy === "asc" ? "A-Z" : "Z-A"}</span>
+            </button>
+          </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {/* Person card */}
-            {people.map(person => (
-              <PersonCard data={person} key={person.id}/>
+            {dataPeople.map(person => (
+              <PersonCard data={person} key={person.id} clickOnHeart={clickOnHeart}/>
             ))}
           </div>
+          {isFilterPageOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+          {/* Add your filter/sort page content and logic here */}
+          <div className="bg-white rounded-lg p-4">
+            <h3 className="text-xl font-bold mb-2">Filter/Sort Page</h3>
+            {/* Add your filter/sort options here */}
+            {/* Close button */}
+            <button
+              onClick={() => setIsFilterPageOpen(false)}
+              className="text-white bg-gray-900 rounded-md px-3 py-1 focus:outline-none"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
         </div>
       </main>
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
