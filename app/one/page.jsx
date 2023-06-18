0003882ba87e628abd54a1494ad0d165f865b79a
miillaadddddd <milad7212@gmail.com>
2023-06-18 11:04:55 +0330

    Add filter modal

--- app/one/page.jsx
+++ app/one/page.jsx
@@ -1,10 +1,13 @@
-"use client";
-import { useCallback, useState } from "react";
-import Header from "../components/header";
-import PersonCard from "../components/personCard";
-import { RiFilter2Fill, RiArrowUpDownFill } from "react-icons/ri";
+"use client"
 
-function App() {
+
+
+import React, { useCallback, useState } from 'react';
+import Header from '../components/header';
+import PersonCard from '../components/personCard';
+import { RiFilter2Fill, RiArrowUpDownFill } from 'react-icons/ri';
+
+const App = () => {
   const [dataPeople, setDataPeople] = useState([
     {
       id: 1,
@@ -77,103 +80,191 @@ function App() {
     // Add more people data
   ]);
 
+  const [filteredData, setFilteredData] = useState(dataPeople);
+  const [sortBy, setSortBy] = useState('');
   const [filterFavorites, setFilterFavorites] = useState(false);
-  const [sortBy, setSortBy] = useState("");
   const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false);
 
   const clickOnHeart = useCallback(
     (selectedCard) => {
-      const index = dataPeople.findIndex(
-        (person) => person.id === selectedCard.id
-      );
-      if (index !== -1) {
-        const updatedPeople = [...dataPeople];
-        updatedPeople[index].favorite = !updatedPeople[index].favorite;
-        setDataPeople(updatedPeople);
-      }
+      const updatedPeople = dataPeople.map((person) => {
+        if (person.id === selectedCard.id) {
+          return { ...person, favorite: !person.favorite };
+        }
+        return person;
+      });
+      setDataPeople(updatedPeople);
     },
     [dataPeople]
   );
 
   const handleFilterFavorites = () => {
-    setFilterFavorites((prevState) => !prevState);
-    setIsFilterMenuOpen(true);
+    setFilterFavorites(!filterFavorites);
   };
 
   const handleSortBy = () => {
-    if (sortBy === "asc") {
-      setSortBy("desc");
-    } else {
-      setSortBy("asc");
-    }
-    setIsFilterMenuOpen(true);
+    const sortOrder = sortBy === 'asc' ? 'desc' : 'asc';
+    setSortBy(sortOrder);
   };
 
-  const filteredAndSortedPeople = dataPeople
+  const handleFilterByName = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    const filteredPeople = dataPeople.filter((person) =>
+      person.name.toLowerCase().includes(searchValue)
+    );
+    setFilteredData(filteredPeople);
+  };
+
+  const handleFilterByHasLikeFromMe = () => {
+    const filteredPeople = dataPeople.filter((person) => person.favorite);
+    setFilteredData(filteredPeople);
+  };
+
+  const handleFilterByHasLike = () => {
+    const filteredPeople = dataPeople.filter((person) => person.liked > 0);
+    setFilteredData(filteredPeople);
+  };
+
+  const handleFilterByHasComment = () => {
+    const filteredPeople = dataPeople.filter((person) => person.comments.length > 0);
+    setFilteredData(filteredPeople);
+  };
+
+  const handleFilterByHasNoteFromMe = () => {
+    const filteredPeople = dataPeople.filter((person) =>
+      person.notes.some((note) => note.writer.name === 'Your Name')
+    );
+    setFilteredData(filteredPeople);
+  };
+
+  const handleFilterByJob = (job) => {
+    const filteredPeople = dataPeople.filter((person) => person.skills.includes(job));
+    setFilteredData(filteredPeople);
+  };
+
+  const handleSortByAlphabet = () => {
+    const sortedPeople = [...filteredData].sort((a, b) => a.name.localeCompare(b.name));
+    setFilteredData(sortedPeople);
+    setSortBy('alphabet');
+  };
+
+  const handleSortByLiked = () => {
+    const sortedPeople = [...filteredData].sort((a, b) => a.liked - b.liked);
+    setFilteredData(sortedPeople);
+    setSortBy('liked');
+  };
+
+  const handleSortByCommentCount = () => {
+    const sortedPeople = [...filteredData].sort(
+      (a, b) => a.comments.length - b.comments.length
+    );
+    setFilteredData(sortedPeople);
+    setSortBy('commentCount');
+  };
+
+  const filteredAndSortedPeople = filteredData
     .filter((person) => !filterFavorites || person.favorite)
     .sort((a, b) => {
-      if (sortBy === "asc") {
+      if (sortBy === 'asc') {
         return a.name.localeCompare(b.name);
-      } else if (sortBy === "desc") {
+      } else if (sortBy === 'desc') {
         return b.name.localeCompare(a.name);
       }
       return 0;
     });
 
+  const openFilterModal = () => {
+    setIsFilterMenuOpen(true);
+  };
+
+  const closeFilterModal = () => {
+    setIsFilterMenuOpen(false);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Header */}
       <Header />
-
-      {/* Main content */}
       <main className="flex-grow">
         <div className="container mx-auto py-4 px-6">
           <div className="flex justify-between mb-4">
             <button
-              onClick={handleFilterFavorites}
+              onClick={openFilterModal}
               className="flex items-center space-x-1 px-3 py-1 bg-gray-900 text-white rounded-lg focus:outline-none"
             >
               <RiFilter2Fill size={18} />
-              <span>{filterFavorites ? "All" : "Favorites"}</span>
+              <span>{filterFavorites ? 'All' : 'Favorites'}</span>
             </button>
             <button
               onClick={handleSortBy}
               className="flex items-center space-x-1 px-3 py-1 bg-gray-900 text-white rounded-lg focus:outline-none"
             >
               <RiArrowUpDownFill size={18} />
-              <span>{sortBy === "asc" ? "A-Z" : "Z-A"}</span>
+              <span>{sortBy === 'asc' ? 'A-Z' : 'Z-A'}</span>
             </button>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {/* Person card */}
             {filteredAndSortedPeople.map((person) => (
-              <PersonCard
-                data={person}
-                key={person.id}
-                clickOnHeart={clickOnHeart}
-              />
+              <PersonCard data={person} key={person.id} clickOnHeart={clickOnHeart} />
             ))}
           </div>
           {isFilterMenuOpen && (
-            <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
-              {/* Add your filter/sort menu content and logic here */}
-              <div className="bg-white rounded-lg p-4">
-                <h3 className="text-xl font-bold mb-2">Filter/Sort Menu</h3>
-                {/* Add your filter/sort options here */}
-                {/* Close button */}
-                <button
-                  onClick={() => setIsFilterMenuOpen(false)}
-                  className="text-white bg-gray-900 rounded-md px-3 py-1 focus:outline-none"
-                >
-                  Close
-                </button>
-              </div>
-            </div>
+           <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+           <div className="bg-white rounded-lg p-4 w-96">
+             <div className="space-y-4">
+               <input
+                 type="text"
+                 placeholder="Filter by name"
+                 className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none w-full"
+                 onChange={handleFilterByName}
+               />
+               <button
+                 onClick={handleFilterByHasLikeFromMe}
+                 className="btn-filter"
+               >
+                 Has Like from Me
+               </button>
+               <button onClick={handleFilterByHasLike} className="btn-filter">
+                 Has Like
+               </button>
+               <button onClick={handleFilterByHasComment} className="btn-filter">
+                 Has Comment
+               </button>
+               <button onClick={handleFilterByHasNoteFromMe} className="btn-filter">
+                 Has Note from Me
+               </button>
+               <select
+                 onChange={(e) => handleFilterByJob(e.target.value)}
+                 className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none w-full"
+               >
+                 <option value="">All Jobs</option>
+                 <option value="مهندس عمران">مهندس عمران</option>
+                 <option value="برقکار">برقکار</option>
+                 <option value="تراکتور">تراکتور</option>
+               </select>
+               <button onClick={handleSortByAlphabet} className="btn-filter">
+                 Sort by Alphabet
+               </button>
+               <button onClick={handleSortByLiked} className="btn-filter">
+                 Sort by Liked Count
+               </button>
+               <button onClick={handleSortByCommentCount} className="btn-filter">
+                 Sort by Comment Count
+               </button>
+             </div>
+             <button
+               onClick={closeFilterModal}
+               className="text-white bg-gray-900 rounded-md px-3 py-2 focus:outline-none mt-4"
+             >
+               Close
+             </button>
+           </div>
+         </div>
+         
           )}
         </div>
       </main>
     </div>
   );
-}
+};
 
 export default App;
