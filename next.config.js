3ff3e93eb155f5b61e29c858864284d635dac586
miillaadddddd <milad7212@gmail.com>
2023-07-23 14:19:09 +0330

    remove pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pwa: {
-        name: 'My PWA',
-        shortName: 'MyPWA',
-        icon: '/favicon.ico',
-        themeColor: '#000000',
-        manifest: {
-          start_url: '/',
-          background_color: '#ffffff',
-        },
-      },
+ 
 }
 
 module.exports = nextConfig
