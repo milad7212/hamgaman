49b8efa9a25454d41ceef5c3034391ea5c38cc3f
miillaadddddd <milad7212@gmail.com>
2023-07-31 14:12:18 +0330

    Add delete Icon

--- app/components/javadi/EmptyCard.jsx
+++ app/components/javadi/EmptyCard.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { BsPlusCircle } from "react-icons/bs";
+import {AiOutlineDelete} from 'react-icons/ai'
 function EmptyCard({ children,whichIndex }) {
   return (
     <div className="my-8 m-4 border  relative border-dashed border-gray-400 rounded-md h-28 hover:shadow-xl duration-200 ease-in cursor-pointer hover:border-solid">
+      <div className=" absolute left-2 top-2">
+      <AiOutlineDelete className="text-gray-800 hover:text-red-500 hover:scale-125 duration-150 ease-in"/>
+      </div>
+
       <div className=" left-0 right-0 flex justify-center absolute -top-3">
         <BsPlusCircle onClick={()=>alert(whichIndex)}  size={25} className="text-gray-400 hover:text-gray-700" />
       </div>
