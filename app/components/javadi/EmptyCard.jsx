54e3d8e4b8d2cba956d9645b9a5ba4c0f16438d1
miillaadddddd <milad7212@gmail.com>
2023-07-31 14:08:24 +0330

    change ui

--- app/components/javadi/EmptyCard.jsx
+++ app/components/javadi/EmptyCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BsPlusCircle } from "react-icons/bs";
-function EmptyCard({ children }) {
+function EmptyCard({ children,whichIndex }) {
   return (
     <div className="my-8 m-4 border  relative border-dashed border-gray-400 rounded-md h-28 hover:shadow-xl duration-200 ease-in cursor-pointer hover:border-solid">
       <div className=" left-0 right-0 flex justify-center absolute -top-3">
-        <BsPlusCircle size={25} className="text-gray-400 hover:text-gray-700" />
+        <BsPlusCircle onClick={()=>alert(whichIndex)}  size={25} className="text-gray-400 hover:text-gray-700" />
       </div>
       <div className=" left-0 right-0 flex justify-center absolute -bottom-3">
         <BsPlusCircle size={25} className="text-gray-400 hover:text-gray-700" />
--- app/javadi/page.jsx
+++ app/javadi/page.jsx
@@ -3,16 +3,67 @@
 import React, { useState } from "react";
 import EmptyCard from "../components/javadi/EmptyCard";
 import InputSample from "../components/javadi/InputSample";
+import {VscPreview} from 'react-icons/vsc'
 
 function Page() {
-  const [dataForm, setDataForm] = useState([]);
+  const [dataForm, setDataForm] = useState([
+    {
+      type: "InputSample",
+      data: {
+        label: "عنوان",
+        placeholder: "متن خود را وارد نمایید",
+      },
+    },
+    {
+      type: "InputSample",
+      data: {
+        label: "عنوان",
+        placeholder: "متن خود را وارد نمایید",
+      },
+    },
+    {
+      type: "InputSample",
+      data: {
+        label: "عنوان",
+        placeholder: "متن خود را وارد نمایید",
+      },
+    },
+  ]);
+
+  function generateForm(dataForm) {
+    return dataForm.map((item,index) => {
+      switch(item.type) {
+
+        case 'InputSample':
+          return (
+            <EmptyCard whichIndex={index} key={item.data.label}>
+              <InputSample 
+                label={item.data.label}
+                placeholder={item.data.placeholder} 
+              />
+            </EmptyCard>
+          );
+  
+        default:
+          return null;
+      }
+    });
+  }
+  
+ 
+  
+  
   return (
     <div className="bg-gray-200 min-h-screen">
-      <div className="flex justify-center py-4">
+      <div className="flex relative justify-center py-4">
+        <span className="absolute left-6">
+          <VscPreview size={25} className="text-gray-600 cursor-pointer hover:scale-105 duration-200 ease-in hover:text-gray-900"/>
+        </span>
         {/* <span className='font-bold text-blue-600 text-xl'>فرم ساز آنلاین</span> */}
       </div>
       <div className="">
-        <EmptyCard />
+        {generateForm(dataForm)}
+
         <EmptyCard>
           <InputSample />
         </EmptyCard>
