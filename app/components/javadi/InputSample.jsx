52c8a4db21b18806a9ac438e31e44cfcebf11f75
miillaadddddd <milad7212@gmail.com>
2023-07-31 14:58:53 +0330

    Add NewForm

--- app/components/javadi/InputSample.jsx
+++ app/components/javadi/InputSample.jsx
@@ -4,7 +4,7 @@ function InputSample({label='عنوان',placeholder='متن راهنما'}) {
   return (
     <div className="m-2 my-4">
         <p className="text-sm mb-2 text-gray-600">{label}</p>
-        <input className=' w-full border rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text"   placeholder={placeholder}/>
+        <input className=' w-full border bg-gray-100 rounded-md py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text"   placeholder={placeholder}/>
     </div>
   )
 }
--- app/javadi/page.jsx
+++ app/javadi/page.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import EmptyCard from "../components/javadi/EmptyCard";
 import InputSample from "../components/javadi/InputSample";
-import {VscPreview} from 'react-icons/vsc'
+import { VscPreview } from "react-icons/vsc";
 
 function Page() {
   const [dataForm, setDataForm] = useState([
@@ -31,43 +31,62 @@ function Page() {
   ]);
 
   function generateForm(dataForm) {
-    return dataForm.map((item,index) => {
-      switch(item.type) {
-
-        case 'InputSample':
+    return dataForm.map((item, index) => {
+      switch (item.type) {
+        case "InputSample":
           return (
-            <EmptyCard whichIndex={index} key={item.data.label}>
-              <InputSample 
+            <EmptyCard
+              onFormAddDelete={onFormAddDelete}
+              whichIndex={index}
+              key={item.data.label}
+            >
+              <InputSample
                 label={item.data.label}
-                placeholder={item.data.placeholder} 
+                placeholder={item.data.placeholder}
               />
             </EmptyCard>
           );
-  
+
         default:
           return null;
       }
     });
   }
-  
- 
-  
-  
+
+  function onFormAddDelete(data) {
+    const newDataForm = [...dataForm];
+    switch (data.type) {
+      case "add":
+        newDataForm.splice(data.indexItem, 0, {
+          type: "InputSample",
+          data: {
+            label: "عنوان",
+            placeholder: "متن خود را وارد نمایید",
+          },
+        });
+        setDataForm(newDataForm);
+
+        break;
+
+      case "delete":
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="bg-gray-200 min-h-screen">
       <div className="flex relative justify-center py-4">
         <span className="absolute left-6">
-          <VscPreview size={25} className="text-gray-600 cursor-pointer hover:scale-105 duration-200 ease-in hover:text-gray-900"/>
+          <VscPreview
+            size={25}
+            className="text-gray-600 cursor-pointer hover:scale-105 duration-200 ease-in hover:text-gray-900"
+          />
         </span>
         {/* <span className='font-bold text-blue-600 text-xl'>فرم ساز آنلاین</span> */}
       </div>
-      <div className="">
-        {generateForm(dataForm)}
-
-        <EmptyCard>
-          <InputSample />
-        </EmptyCard>
-      </div>
+      <div className="">{generateForm(dataForm)}</div>
     </div>
   );
 }
